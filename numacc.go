94804e5a380d21ee69d9a3ac841387c1279509ef
mjace <b436412@gmail.com>
2019-08-06 07:52:10 -0700

    can get a container's process and it's cpu id

--- numacc.go
+++ numacc.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -48,17 +49,53 @@ func run(c *cli.Context) error {
 func numacc() error {
 	fmt.Println("Container ID:", config.containerID)
 
-	cmd := exec.Command("ps", "-o", "psr" ,"-p", config.containerID)
-	fmt.Println(cmd)
+	log.Println("initPidMapByContainerId")
+	pidCpuMap := initPidMapByContainerID(config.containerID)
+	//fmt.Println(pidCpuMap)
+	//log.Println("fill cpu id by pid map")
+	fillCpuIdByPidMap(pidCpuMap)
+	fmt.Println(pidCpuMap)
+	return nil
+}
 
+func getCpuIDByPid(id string) string {
+	cmd := exec.Command("ps", "-o", "psr" ,"-p", id)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		//fmt.Println(out)
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	//fmt.Printf("combined out:\n%s\n", string(out))
+
+	cpuID := strings.Split(string(out),"\n")[1]
+	cpuID = strings.TrimPrefix(cpuID, "  ")
+	cpuID = strings.TrimSuffix(cpuID, " ")
+	return cpuID
+}
+
+func initPidMapByContainerID(id string) map[string]string {
+	log.Println("init Pid Map by container ID...")
+	pidCpuMap := make(map[string]string)
+
+	cmd := exec.Command("docker", "top", config.containerID)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
+	for counter := 1; counter < len(strings.Split(string(out), "\n"))-1; counter++ {
+		tmpId := strings.Split(string(out),"\n")[counter]
+		re, _ := regexp.Compile("\\s(\\w+)\\S")
+		pId := re.FindStringSubmatch(tmpId)
+		pId[0] = strings.TrimPrefix(pId[0], " ")
+		pId[0] = strings.TrimSuffix(pId[0], " ")
+		pidCpuMap[pId[0]] = "0"
+	}
+	return pidCpuMap
+}
 
-	cpuID := strings.Split(string(out),"\n")[1]
-	fmt.Println("cpu ID : ", cpuID)
-
-	return nil
+func fillCpuIdByPidMap(inputMap map[string]string){
+	for k := range inputMap {
+		inputMap[k] = getCpuIDByPid(k)
+	}
 }
